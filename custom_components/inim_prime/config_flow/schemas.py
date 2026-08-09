f7c6df85dbf0edd72bac2ecba6ee7b76346bec78
Pitscheider <pitscheider@tuta.com>
2026-08-09 15:03:38 +0200

    Time intervals are set to default when user is starting using a new backend which requires different timings

--- custom_components/inim_prime/config_flow/schemas.py
+++ custom_components/inim_prime/config_flow/schemas.py
@@ -204,7 +204,7 @@ def build_options_schema(
     if current_options is not None:
         # In case the user is migrating from native or primelan does not use the existing intervals for zones and partitions
         # This is to help the user who does not know that a different backend has different timings
-        if migrating_from_native or migrating_from_primelan:
+        if (migrating_from_native and has_primelan and not has_native) or (migrating_from_primelan and has_native):
             scan_intervals_defaults["zones"] = current_options.zones_scan_interval if current_options.zones_scan_interval is not None else scan_intervals_defaults["zones"]
             scan_intervals_defaults["partitions"] = current_options.partitions_scan_interval if current_options.partitions_scan_interval is not None else scan_intervals_defaults["partitions"]
         scan_intervals_defaults["gsm"] = current_options.gsm_scan_interval if current_options.gsm_scan_interval is not None else scan_intervals_defaults["gsm"]
